ec118d1b66e0c1d4ea81364a57e098e27fec7dcd
NorMenMe <info@normanmetz.com>
2021-07-29 09:46:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/scss/main.scss";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar className="Navbar" />
+      </BrowserRouter>
     </div>
   );
 }
